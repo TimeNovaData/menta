e005ece4afb342550b014b361cc9c47cd08603a8
Clara Moreira <anacacamm@gmail.com>
2022-11-23 09:50:58 -0300

    feat: create post page

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import getElementAndappend from "./modules/getElementAndappend.js";
 
 const PageEmBreve = document.querySelector("#PageEmBreve");
 const PageFaq = document.querySelector("#PageFaq");
+const PagePost = document.querySelector("#PagePost");
 
 getElementAndappend(".svg-container", "../../templates/parts/svg.html");
 
@@ -9,11 +10,11 @@ if (PageFaq) {
   document.body.classList.add("PageFaq");
   getElementAndappend(".footer-container", "../../templates/parts/footer.html");
   getElementAndappend(".menu-container", "../../templates/parts/menu.html");
+} else if (PageEmBreve) {
+  document.body.classList.add("PageEmBreve");
+  getElementAndappend(".menu-container", "../../templates/parts/menu.html");
+} else if (PagePost) {
+  document.body.classList.add("PagePost");
+  getElementAndappend(".menu-container", "../../templates/parts/menu.html");
+  getElementAndappend(".footer-container", "../../templates/parts/footer.html");
 }
-
-
-else if (PageEmBreve) {
-    document.body.classList.add("PageEmBreve");
-    getElementAndappend(".menu-container", "../../templates/parts/menu.html");
-}
-
