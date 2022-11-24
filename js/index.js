e98b19deeb4564bb7206b36b0a2b05dc14909cdf
Clara Moreira <anacacamm@gmail.com>
2022-11-24 13:59:55 -0300

    feat: update post and blog pages with Swiper

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import getElementAndappend from "./modules/getElementAndappend.js";
 const PageEmBreve = document.querySelector("#PageEmBreve");
 const PageFaq = document.querySelector("#PageFaq");
 const PagePost = document.querySelector("#PagePost");
+const PageBlog = document.querySelector("#PageBlog");
 
 getElementAndappend(".svg-container", "../../templates/parts/svg.html");
 
@@ -17,9 +18,24 @@ if (PageFaq) {
   document.body.classList.add("PagePost");
   getElementAndappend(".menu-container", "../../templates/parts/menu.html");
   getElementAndappend(".footer-container", "../../templates/parts/footer.html");
+} else if (PageBlog) {
+  document.body.classList.add("PageBlog");
+  getElementAndappend(".menu-container", "../../templates/parts/menu.html");
+  getElementAndappend(".footer-container", "../../templates/parts/footer.html");
 }
 
-const swiper = new Swiper(".swiper", {
-  slidesPerView: 2,
-  spaceBetween: 30,
+const swiper = new Swiper(".swiper-cards", {
+  slidesPerView: 1.2,
+  spaceBetween: 32,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2.5,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
