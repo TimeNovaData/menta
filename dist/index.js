630d7c3c5bc2163a9b123d77ad47ad85fd65ce21
jhonatan <jhowfrombrazil@gmail.com>
2023-01-26 18:12:12 -0300

    aa

--- dist/index.js
+++ dist/index.js
@@ -873,6 +873,76 @@ function AnimationHome() {
         // gsap.set('#banner-hero', {
         //   y:'-150vh'
         // })
+        //   let second = document.querySelector(".to-stay"),
+        // child = document.querySelector(".plane-wrapper");
+        //   gsap.to(".plane-wrapper", {
+        //     y: () => {
+        //       let containerBounds = second.getBoundingClientRect(),
+        //           textBounds = child.getBoundingClientRect(),
+        //           center = containerBounds.top + containerBounds.height / 2;
+        //       return center - textBounds.top;
+        //     },
+        //     ease: "none",
+        //     scrollTrigger: {
+        //       trigger: ".plane-wrapper",
+        //       start: "top center",
+        //       end: "+=50%",
+        //       invalidateOnRefresh: true,
+        //       scrub: true
+        //     }
+        //   });
+        gsap.set(".ceu-banner-2", {
+            y: "-1900px"
+        });
+        gsap.set(".plane-wrapper img", {
+            x: -100
+        }, "<");
+        gsap.to(".plane-wrapper", {
+            //  y: () => window.innerHeight / 4 * -1,
+            ease: "none",
+            // x: '50%',
+            scrollTrigger: {
+                trigger: "#banner-hero-2",
+                start: "top top",
+                endTrigger: "#banner-hero-2",
+                end: "bottom-=100px bottom",
+                pin: ".plane-wrapper",
+                pinSpacing: false,
+                invalidateOnRefresh: true,
+                anticipatePin: 1
+            }
+        });
+        let tl3 = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".to-stay",
+                markers: true,
+                toggleActions: "play pause",
+                start: "top",
+                //   // start: "top",
+                end: "bottom-=100px bottom",
+                pin: ".to-stay",
+                // anticipatePin: 1,
+                scrub: 2,
+                pinSpacing: false,
+                endTrigger: "#banner-hero-2",
+                onUpdate ({ progress , direction , isActive  }) {
+                // console.log(progress)              
+                }
+            }
+        });
+        tl3.to(".ceu-banner-2", {
+            y: ()=>1900 - Math.round(window.innerHeight) + "px"
+        }).to(".ceu-banner-2 img", {
+            scale: 1
+        }, "<").to(".plane-wrapper img", {
+            x: 100
+        }, "<").to(".pista img", {
+            ["object-position"]: "70%",
+            y: 500
+        }, "<").to(".segundo-plano img", {
+            ["object-position"]: "50%",
+            y: 600
+        }, "<");
         return;
     //  tl2.from('.parallax-group-images', {
     //    y: 100
